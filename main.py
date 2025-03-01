70cf538a322455a388de6cbf3e3b19ca840da47d
hohongtruong <hohongtruong2910@gmail.com>
2025-03-02 00:20:21 +0700

    2025-03-02 00:20:21 lawPA8SiKXUV5YM78fYqSruLQd3iHeiWwhY0qo5GD2TK7RaRbgjd6HAfpY0ARqFSPgw7kOsNGByQUXJztOXkdNK87gklk8CiHWrLgh9Gdm7l1UEeO5f3gZMZ1Cjft6Uj

--- main.py
+++ main.py
@@ -4,23 +4,18 @@ import random
 import time
 from datetime import datetime
 
-def create_new_commit():
-    text = np.random.choice(list(string.ascii_letters+string.digits),1000000,replace=True)
-    text = ''.join(text)
+def create_new_commit(char = string.ascii_letters+string.digits):
+    text = ''.join(random.choice(char) for _ in range(1000000))
     with open('spam.txt','w') as f:
         f.write(text)
 
     os.system('git add . ')
 
-    commit_name = np.random.choice(list(string.ascii_letters+string.digits),128,replace=True)
-    commit_name = ''.join(commit_name)
+    commit_name = ''.join(random.choice(char) for _ in range(128))
     commit_name = datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ' ' + commit_name
 
-
     os.system('git commit -m "%s"'%commit_name)
-
     os.system('git push origin main')
-
 
 for i in range(10):
     create_new_commit()
