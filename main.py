f15414146596f24d7304584695b3259172a4cfaf
hohongtruong <hohongtruong2910@gmail.com>
2025-03-01 23:55:32 +0700

    2025-03-01 23:55:32 r0RMz8YMGvB2eII489r332CPdrT1DUsLsk7c40VjZqUxdNYvhtyNej5fSaqjyqwNPS5S2VwaXhFuQcw7I5Oy69Tihq1xpz4YBYoiDmOCnAfUSMES8KfVMkGkvhFOTsOx

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
+import os
 import string
-import random
 import numpy as np
+import time
+from datetime import datetime
 
-text = np.random.choice(list(string.ascii_letters+string.digits),1000000,replace=True)
-text = ''.join(text)
-with open('spam.txt','w') as f:
-    f.write(text)
+def create_new_commit():
+    text = np.random.choice(list(string.ascii_letters+string.digits),1000000,replace=True)
+    text = ''.join(text)
+    with open('spam.txt','w') as f:
+        f.write(text)
+
+    os.system('git add . ')
+
+    commit_name = np.random.choice(list(string.ascii_letters+string.digits),128,replace=True)
+    commit_name = ''.join(commit_name)
+    commit_name = datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ' ' + commit_name
+
+
+    os.system('git commit -m "%s"'%commit_name)
+
+    os.system('git push origin main')
+
+
+while True:
+    create_new_commit()
+    time.sleep(5*60)
